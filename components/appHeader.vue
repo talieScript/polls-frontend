2bc389fca0441fdb1949c331be6d2ddcb65f9386
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-10-25 15:38:44 +0000

    refactored header

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="bg-white flex-auto flex flex-row p-3 items-center justify-center shadow-xs">
-    <a href="/" class="w-30 w-xp font-bold text-xl justify-self-start mr-auto text-black">
+  <header class="bg-white flex-auto flex flex-row p-3 items-center justify-between shadow-xs">
+    <a href="/" class="w-30 w-xp font-bold text-xl justify-self-start text-black">
       <h1>Poll <span class="text-primary">Simply</span></h1>
     </a>
     <nav class="font-semibold justify-self-center">
       <a class="mr-3 hover:text-gray-500 transition-colors duration-200" href="/poll-list">Poll List</a>
       <a href="/new" class=" text-green-400 hover:text-green-300 transition-colors duration-200">Create New</a>
     </nav>
-    <div class="justify-end ml-auto">
+    <div class="justify-end">
       <button class="mr-2 hover:text-gray-500 transition-colors duration-200">
         Login
       </button>
