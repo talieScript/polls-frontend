c8bab7372dcb5066dc68373e35fbff00d9161945
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-10-26 19:02:37 +0000

    Made header fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="font-sans bg-secondary h-screen text-gray-600">
     <AppHeader />
-    <Nuxt />
+    <Nuxt class="pt-16 sm:pt-16" />
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from 'vue'
 
-  export default Vue.extend({
-    
-  })
+export default Vue.extend({})
 </script>
 
 <style scoped>
-
 </style>
