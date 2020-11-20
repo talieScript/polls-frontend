dcbf13366adbef0f72c6da0c0860362ff0c8f907
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-20 15:21:57 +0000

    inported fonts properly

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700&family=Roboto:wght@300;400;500&display=swap'
+      }
     ]
   },
 
