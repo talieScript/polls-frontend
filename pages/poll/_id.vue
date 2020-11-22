cd7a2d5f90713067df6095d4895261d160d2a597
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-22 17:19:07 +0000

    Added poll page and got id from url

---
+++ pages/poll/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ this.id }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ params }) {
+    const id = params.id
+    return {
+      id,
+    }
+  },
+})
+</script>
+
+<style scoped>
+</style>
