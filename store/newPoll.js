87efef3128d7936e588925c61225f26b5a01d4fe
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-23 20:46:49 +0000

    push router when submitting new poll

--- store/newPoll.js
+++ store/newPoll.js
@@ -31,13 +31,12 @@ export const mutations = {
 export const actions = {
   async submit({state}, password) {
     const payload = createPostPayload(state.active);
-    console.log(payload)
     this.$axios.$post('/polls', {
       ...payload,
       password,
     }).then((createdPoll) => {
       console.log('Poll created! 🥳');
-      this.$router.push()
+      this.$router.push('/poll/' + createdPoll)
     }).catch(error => {
       console.log(error);
     })
