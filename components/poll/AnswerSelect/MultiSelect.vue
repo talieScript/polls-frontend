7859f92f344e3b4f6962a981b19dc250de9eb462
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-27 21:01:54 +0000

    handled check

--- components/poll/AnswerSelect/MultiSelect.vue
+++ components/poll/AnswerSelect/MultiSelect.vue
@@ -1,10 +1,21 @@
 <template>
   <fieldset>
     <div v-for="answer in answers" :key="answer.id" class="flex mt-5">
-      <input v-model="chosen" :id="answer.id" type="checkbox" class="w-0 h-0" />
-      <div class="box transition-all duration-200">
-        <label :for="answer.id" class="label mr-auto">
+      <input
+        v-model="checkedAnswers"
+        :id="answer.id"
+        type="checkbox"
+        class="w-0 h-0"
+        :value="answer.id"
+        :checked="checkedAnswers.includes(answer.id)"
+      />
+      <div
+        class="box transition-all duration-200"
+        @click="toggleCheck(answer.id)"
+      >
+        <label :for="answer.id" class="label mr-auto pointer-events-none">
           {{ answer.answer_string }}
+          {{ checkedAnswers.includes(answer.id) ? 'here' : 'no' }}
         </label>
         <span class="w-6 h-6 border rounded bg-gray-100"></span>
       </div>
@@ -13,11 +24,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { PropOptions } from 'vue'
 import { ApiAnswer } from '@/utils/types'
 
 export default Vue.extend({
   name: 'MultiSelect',
+  data(): any {
+    return {}
+  },
   props: {
     answers: {
       type: Array,
@@ -26,24 +40,31 @@ export default Vue.extend({
     value: {
       type: Array,
       required: true,
-    },
+    } as PropOptions<string[]>,
     answerNumber: {
       type: Number,
       required: true,
     },
   },
-  created() {
-    console.log(this.answers)
-  },
   computed: {
-    chosen: {
+    checkedAnswers: {
       get(): string[] {
         return this.value
       },
       set(value: ApiAnswer) {
-        this.$emit('input', value.id)
+        this.$emit('input', value)
       },
     } as any,
+  },
+  methods: {
+    toggleCheck(id): void {
+      const answerIndex = this.checkedAnswers.indexOf(id)
+      if (answerIndex + 1) {
+        this.checkedAnswers.splice(answerIndex, 1)
+      } else {
+        this.checkedAnswers.push(id)
+      }
+    },
   },
 })
 </script>
