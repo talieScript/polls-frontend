2e2ff1feb958fb5d32f38cd9963bdbba07c3235d
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-12-29 19:51:57 +0000

    reated snacbar compenent

---
+++ components/SanckBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SnackBar',
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    open: {
+      get(): boolean {
+        return this.value
+      },
+      set(value: boolean) {
+        this.$emit('input', value)
+      },
+    },
+  },
+})
+</script>
+
+<style scoped>
+</style>
